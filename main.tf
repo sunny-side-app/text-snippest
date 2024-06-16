69b7ba32e3cf4ec534c1946b3161cdf81efd02d7
Takahiro Ido <juve.taka@gmail.com>
2024-06-16 07:01:53 +0000

    Fix SSH private key handling and add debug steps

--- main.tf
+++ main.tf
@@ -22,7 +22,7 @@ resource "null_resource" "deploy" {
     connection {
       type        = "ssh"
       user        = "ubuntu" # SSHユーザー名
-      private_key = var.ssh_private_key # 秘密鍵を使用して認証します
+      private_key = file(ssh_private_key) # 秘密鍵を使用して認証します
       host        = data.aws_instance.existing_instance.public_ip # インスタンスのパブリックIP
     }
   }
@@ -45,7 +45,7 @@ resource "null_resource" "deploy" {
     connection {
       type        = "ssh"
       user        = "ubuntu" # SSHユーザー名
-      private_key = var.ssh_private_key # 秘密鍵を使用して認証します
+      private_key = file(ssh_private_key) # 秘密鍵を使用して認証します
       host        = data.aws_instance.existing_instance.public_ip # インスタンスのパブリックIP
       timeout     = "10m"
       agent       = false
